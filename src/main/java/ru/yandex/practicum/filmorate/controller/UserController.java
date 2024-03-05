6ec8859e7af74fe31c067210ea6e7538085177e1
aleksey <alexx308@mail.ru>
2024-03-05 23:08:16 +0400

    feat: реализовано добавление друзей

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -26,7 +26,7 @@ public class UserController {
     }
 
     @PutMapping("/users")
-    public ResponseEntity<User> changeFilm(@Valid @RequestBody User user) {
+    public ResponseEntity<User> changeUser(@Valid @RequestBody User user) {
         log.info("PUT /users: {}", user.toString());
         return new ResponseEntity<>(userService.changeUser(user), HttpStatus.OK);
     }
@@ -34,5 +34,33 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUsers());
+    }
+
+    @PutMapping("/users/{id}/friends/{friendId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void addFriends(@PathVariable Long id, @PathVariable Long friendId) {
+        log.info("PUT /friends: {}, {}", id, friendId);
+        userService.addFriends(id, friendId);
+    }
+
+    @DeleteMapping("/users/{id}/friends/{friendId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteFriends(@PathVariable Long id, @PathVariable Long friendId) {
+        log.info("DELETE /friends: {}, {}", id, friendId);
+        userService.deleteFriends(id, friendId);
+    }
+
+    @GetMapping("/users/{id}/friends")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getFriends(@PathVariable Long id) {
+        log.info("GET /friends: {}", id);
+        return userService.getFriends(id);
+    }
+
+    @GetMapping("/users/{id}/friends/common/{otherId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getFriends(@PathVariable Long id, @PathVariable Long otherId) {
+        log.info("GET /friends/common: {}, {}", id, otherId);
+        return userService.getCommonFriends(id, otherId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -9,6 +9,7 @@ import ru.yandex.practicum.filmorate.utils.Validator;
 
 import javax.validation.ValidationException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -41,6 +42,66 @@ public class UserService {
 
     public List<User> getUsers() {
         return new ArrayList<>(userStorage.findAll());
+    }
+
+    public void addFriends(Long id, Long friendId) {
+        if (id.equals(friendId)) {
+            throw new ValidationException("PUT friends: id equals friendId");
+        }
+        var user = userStorage.findById(id);
+        if (user == null) {
+            throw new NotFoundException(String.format("PUT friends: user id %d not found", id));
+        }
+        var friend = userStorage.findById(friendId);
+        if (friend == null) {
+            throw new NotFoundException(String.format("PUT friends: friend id %d not found", friendId));
+        }
+        user.getFriends().add(friendId);
+    }
+
+    public void deleteFriends(Long id, Long friendId) {
+        if (id.equals(friendId)) {
+            throw new ValidationException("DELETE friends: id equals friendId");
+        }
+        var user = userStorage.findById(id);
+        if (user == null) {
+            throw new NotFoundException(String.format("DELETE friends: user id %d not found", id));
+        }
+        var friend = userStorage.findById(friendId);
+        if (friend == null) {
+            throw new NotFoundException(String.format("DELETE friends: friend id %d not found", friendId));
+        }
+        user.getFriends().remove(friendId);
+    }
+
+    public List<User> getFriends(Long id) {
+        var user = userStorage.findById(id);
+        if (user == null) {
+            throw new NotFoundException(String.format("DELETE friends: user id %d not found", id));
+        }
+        List<User> friends = new ArrayList<>();
+        for (var friendId: user.getFriends()) {
+            friends.add(userStorage.findById(friendId));
+        }
+        return friends;
+    }
+
+    public List<User> getCommonFriends(Long id, Long otherId) {
+        var user = userStorage.findById(id);
+        if (user == null) {
+            throw new NotFoundException(String.format("DELETE friends: user id %d not found", id));
+        }
+        var otherUser = userStorage.findById(otherId);
+        if (otherUser == null) {
+            throw new NotFoundException(String.format("DELETE friends: other id %d not found", id));
+        }
+        var commonIds = new HashSet<>(user.getFriends());
+        commonIds.retainAll(otherUser.getFriends());
+        List<User> commonFriends = new ArrayList<>();
+        for (var friendId: commonIds) {
+            commonFriends.add(userStorage.findById(friendId));
+        }
+        return commonFriends;
     }
 
     private long idGenerator() {
