e96530582b5feeb66f7bc300a5fa6b8800b94781
atrefilov <alexx308@mail.ru>
2024-03-06 09:35:56 +0400

    fix: добавлена пустая строка

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
