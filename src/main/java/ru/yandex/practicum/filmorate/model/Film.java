a7219cae20f7a16b606b2e1faa5f1e81feca90f9
atrefilov <alexx308@mail.ru>
2024-03-13 15:16:30 +0400

    feat: добавлены жанры и рейтинги фильмов

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -25,12 +25,16 @@ public class Film {
     private LocalDate releaseDate;
     @Min(1)
     private int duration;
+    private RatingMpa ratingMpa;
     @Setter(AccessLevel.NONE)
     @JsonIgnore
     private final Set<Long> likes;
+    @Setter(AccessLevel.NONE)
+    private final Set<Genre> genre;
 
     public Film() {
         this.likes = new HashSet<>();
+        this.genre = new HashSet<>();
     }
 
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Genre {
+    COMEDY("Комедия"),
+    DRAMA("Драма"),
+    CARTOON("Мультфильм"),
+    THRILLER("Триллер"),
+    DOCUMENTARY("Документальный"),
+    ACTION("Боевик");
+    private final String description;
+
+}
