a873ff4ebe026779550f15d6d77c681a78c08002
atrefilov <alexx308@mail.ru>
2024-03-27 13:06:37 +0400

    feat: получение списка фильмов

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/dao/FilmDbStorage.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.FilmGenre;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.RatingMpa;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
@@ -20,6 +22,7 @@ public class FilmDbStorage implements FilmStorage {
 
     private final JdbcTemplate jdbcTemplate;
     @Override
+    @Transactional
     public Film create(Film film) {
         SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("film")
@@ -35,6 +38,7 @@ public class FilmDbStorage implements FilmStorage {
     }
 
     @Override
+    @Transactional
     public Film update(Film film) {
         String sqlUpdateQuery =
                 "UPDATE film " +
@@ -85,7 +89,36 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Collection<Film> findAll() {
-        return null;
+        String sqlReadFilmQuery = "SELECT f.id,\n" +
+                "    f.name AS film_name,\n" +
+                "    f.description,\n" +
+                "    f.release_date,\n" +
+                "    f.duration,\n" +
+                "    f.rating_id,\n" +
+                "    r.name AS rating_name\n" +
+                "FROM film AS f\n" +
+                "JOIN rating AS r ON f.rating_id = r.id\n" +
+                "ORDER BY f.id;";
+        var films = jdbcTemplate.query(sqlReadFilmQuery, this::makeFilm);
+        String sqlReadGenreQuery = "SELECT f.film_id,\n" +
+                "    f.genre_id,\n" +
+                "    g.name\n" +
+                "FROM genre AS g\n" +
+                "JOIN film_genre AS f ON f.genre_id = g.id\n" +
+                "ORDER BY f.film_id;";
+        var filmGenres = jdbcTemplate.query(sqlReadGenreQuery, this::makeFilmGenre);
+        Map<Long, Set<Genre>> genresMap = new HashMap<>();
+        for (var filmGenre: filmGenres) {
+            genresMap.putIfAbsent(filmGenre.getId(), new HashSet<>());
+            genresMap.get(filmGenre.getId()).add(filmGenre.getGenre());
+        }
+        for (var film: films) {
+            var genres = genresMap.getOrDefault(film.getId(), new HashSet<>());
+            for (var genre: genres) {
+                film.addGenre(genre);
+            }
+        }
+        return films;
     }
 
     @Override
@@ -155,6 +188,12 @@ public class FilmDbStorage implements FilmStorage {
     private Genre makeGenre(ResultSet resultSet, int rowNum) throws SQLException {
         return new Genre(resultSet.getInt("id"),
                 resultSet.getString("name")
+        );
+    }
+
+    private FilmGenre makeFilmGenre(ResultSet resultSet, int rowNum) throws SQLException {
+        return new FilmGenre(resultSet.getLong("film_id"),
+                new Genre(resultSet.getInt("genre_id"), resultSet.getString("name"))
         );
     }
 }
